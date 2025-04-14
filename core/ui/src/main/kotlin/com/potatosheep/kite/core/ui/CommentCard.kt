ce1786ce05c4bdcfb2b28d489f6393fd4ff6911d
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2025-04-14 21:43:08 +0800

    Removed unused imports

--- core/ui/src/main/kotlin/com/potatosheep/kite/core/ui/CommentCard.kt
+++ core/ui/src/main/kotlin/com/potatosheep/kite/core/ui/CommentCard.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
--- feature/post/src/main/kotlin/com/potatosheep/kite/feature/post/PostScreen.kt
+++ feature/post/src/main/kotlin/com/potatosheep/kite/feature/post/PostScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.material3.AssistChip
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
