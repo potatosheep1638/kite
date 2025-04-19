33e619ed43e71c76641d5d11a9eed586d216e446
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2025-04-19 21:23:40 +0800

    Added top app bar to loading screens for subreddit & user pages

--- feature/user/src/main/kotlin/com/potatosheep/kite/feature/user/UserScreen.kt
+++ feature/user/src/main/kotlin/com/potatosheep/kite/feature/user/UserScreen.kt
@@ -16,10 +16,10 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -29,7 +29,9 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.potatosheep.kite.core.common.enums.SortOption
 import com.potatosheep.kite.core.designsystem.ErrorMsg
+import com.potatosheep.kite.core.designsystem.KiteIcons
 import com.potatosheep.kite.core.designsystem.LocalBackgroundColor
+import com.potatosheep.kite.core.designsystem.SmallTopAppBar
 import com.potatosheep.kite.core.model.Post
 
 @Composable
@@ -149,6 +151,16 @@ internal fun UserScreen(
 
         if (isLoading) {
             Scaffold(
+                topBar = {
+                    SmallTopAppBar(
+                        backIcon = KiteIcons.Back,
+                        onBackClick = onBackClick,
+                        title = "",
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = LocalBackgroundColor.current
+                        )
+                    )
+                },
                 containerColor = LocalBackgroundColor.current,
                 contentWindowInsets = WindowInsets(0, 0, 0, 0)
             ) { padding ->
