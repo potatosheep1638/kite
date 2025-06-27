dc335a842c40580b905f93b9cda0f85428fb7f82
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2025-06-27 21:03:37 +0800

    fix(app/feature): migrate back to single NavHost
    Besides being confusing, having two NavHosts has caused issues with state persistence between top level destinations and normal ones.
    
    (cherry picked from commit 1549ebf60312541c2379f0ab97f4566ccaaf1be9)

--- feature/home/src/main/kotlin/com/potatosheep/kite/feature/library/nav/HomeNavigation.kt
+++ feature/home/src/main/kotlin/com/potatosheep/kite/feature/library/nav/HomeNavigation.kt
@@ -1,34 +1,20 @@
 package com.potatosheep.kite.feature.library.nav
 
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
-import androidx.navigation.compose.composable
 import com.potatosheep.kite.feature.library.HomeRoute
-import kotlinx.serialization.Serializable
 
-@Serializable
-data object HomeRoute
-
-fun NavController.navigateToHome(navOptions: NavOptions) =
-    navigate(HomeRoute, navOptions)
-
-fun NavGraphBuilder.homeScreen(
+@Composable
+fun HomeScreen(
+    onBackClick: () -> Unit,
     onSubredditClick: (String) -> Unit,
     onSavedClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    composable<HomeRoute>(
-        enterTransition = { EnterTransition.None },
-        exitTransition = { ExitTransition.None }
-    ) {
-        HomeRoute(
-            onSubredditClick = onSubredditClick,
-            onSavedClick = onSavedClick,
-            modifier = modifier
-        )
-    }
+    HomeRoute(
+        onBackClick = onBackClick,
+        onSubredditClick = onSubredditClick,
+        onSavedClick = onSavedClick,
+        modifier = modifier
+    )
 }
