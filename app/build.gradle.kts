449c343db3379117d0c6977819bcffdd0fd23d78
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2025-06-29 00:45:05 +0800

    build(app): update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     defaultConfig {
         compileSdk = 35
         applicationId = "com.potatosheep.kite"
-        versionCode = 5
-        versionName = "0.2.0-beta"
+        versionCode = 6
+        versionName = "0.2.1-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
