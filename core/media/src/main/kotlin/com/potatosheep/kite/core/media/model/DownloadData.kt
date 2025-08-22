9b8f51bd230135ed68da0a5e9cd497c366c98f20
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2025-08-22 18:43:10 +0800

    docs(core): add KDoc comments to DownloadData flags

--- core/media/src/main/kotlin/com/potatosheep/kite/core/media/model/DownloadData.kt
+++ core/media/src/main/kotlin/com/potatosheep/kite/core/media/model/DownloadData.kt
@@ -11,8 +11,21 @@ data class DownloadData(
     val flags: Int
 ) : Parcelable {
     companion object {
+        /**
+         * Tells the download service that the media file to be downloaded is an image.
+         */
         const val IS_IMAGE = 0x00000001
+
+        /**
+         * Tells the download service that the media file to be downloaded is a video. Set this flag
+         * with the [IS_HLS] flag if the video uses HLS.
+         */
         const val IS_VIDEO = 0x00000002
+
+        /**
+         * If set with the [IS_VIDEO] flag, tells the download service that the video file to be
+         * downloaded is a HLS video.
+         */
         const val IS_HLS = 0x00000004
     }
 }
