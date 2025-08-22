f86b275e60742ad353df1340588661d08c67f81d
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2025-08-22 22:47:07 +0800

    feat(app/feature): add long click to search
    Hold subreddit entry in Home screen to immediately open the search page with the subreddit's scope.

--- feature/home/src/main/kotlin/com/potatosheep/kite/feature/library/nav/HomeNavigation.kt
+++ feature/home/src/main/kotlin/com/potatosheep/kite/feature/library/nav/HomeNavigation.kt
@@ -2,6 +2,7 @@ package com.potatosheep.kite.feature.library.nav
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.potatosheep.kite.core.common.enums.SortOption
 import com.potatosheep.kite.feature.library.HomeRoute
 
 @Composable
@@ -9,12 +10,14 @@ fun HomeScreen(
     onBackClick: () -> Unit,
     onSubredditClick: (String) -> Unit,
     onSavedClick: () -> Unit,
+    onSearchClick: (SortOption.Search, SortOption.Timeframe, String?) -> Unit,
     modifier: Modifier = Modifier
 ) {
     HomeRoute(
         onBackClick = onBackClick,
         onSubredditClick = onSubredditClick,
         onSavedClick = onSavedClick,
+        onSearchClick = onSearchClick,
         modifier = modifier
     )
 }
