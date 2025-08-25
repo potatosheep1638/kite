581b46b67cf0f6998491dc1122e77a3020e8842a
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2025-08-25 17:01:02 +0800

    docs(core): add KDoc comments for Notifier interface
    Refs: 23b1d643

--- core/notification/src/main/kotlin/com/potatosheep/kite/core/notification/Notifier.kt
+++ core/notification/src/main/kotlin/com/potatosheep/kite/core/notification/Notifier.kt
@@ -1,11 +1,26 @@
 package com.potatosheep.kite.core.notification
 
+/**
+ * Interface for classes that create notifications for Kite
+ */
 interface Notifier {
+    /**
+     * Post a download notification
+     *
+     * @param filename the name of the file to be downloaded
+     * @param notificationId the ID of the notification
+     */
     fun postDownloadNotification(
         filename: String,
         notificationId: Int
     )
 
+    /**
+     * Update a download notification
+     *
+     * @param notificationId the ID of the target download notification
+     * @param state the desired state of the target download notification
+     */
     fun updateDownloadNotification(
         notificationId: Int,
         state: Int
