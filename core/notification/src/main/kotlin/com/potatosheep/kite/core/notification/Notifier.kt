06d27b296cff966e563280d5f283cff3532b43b6
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2025-08-25 17:25:32 +0800

    docs(core): update KDoc comments for Notifier interface
    Add KDoc comments for state flags. Edit existing KDoc comments for better clarity.
    
    Refs: a42ec845

--- core/notification/src/main/kotlin/com/potatosheep/kite/core/notification/Notifier.kt
+++ core/notification/src/main/kotlin/com/potatosheep/kite/core/notification/Notifier.kt
@@ -1,11 +1,11 @@
 package com.potatosheep.kite.core.notification
 
 /**
- * Interface for classes that create notifications for Kite
+ * Interface for classes that create notifications for Kite.
  */
 interface Notifier {
     /**
-     * Post a download notification
+     * Post a download notification.
      *
      * @param filename the name of the file to be downloaded
      * @param notificationId the ID of the notification
@@ -16,10 +16,11 @@ interface Notifier {
     )
 
     /**
-     * Update a download notification
+     * Update a download notification.
      *
      * @param notificationId the ID of the target download notification
-     * @param state the desired state of the target download notification
+     * @param state the desired state of the target download notification, such as
+     * [Notifier.STATE_DOWNLOADING_IMAGE]
      */
     fun updateDownloadNotification(
         notificationId: Int,
@@ -27,10 +28,25 @@ interface Notifier {
     )
 
     companion object {
+        /**
+         * State for when image download is in progress.
+         */
         const val STATE_DOWNLOADING_IMAGE = 1
+        /**
+         * State for when video download is in progress.
+         */
         const val STATE_DOWNLOADING_VIDEO = 2
+        /**
+         * State for when audio download is in progress.
+         */
         const val STATE_DOWNLOADING_AUDIO = 4
+        /**
+         * State for when a download is complete.
+         */
         const val STATE_COMPLETE = 5
+        /**
+         * State for when a download is cancelled.
+         */
         const val STATE_STOPPED = 6
     }
 }
