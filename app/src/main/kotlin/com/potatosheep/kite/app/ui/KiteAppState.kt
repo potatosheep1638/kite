9fa7845d13c93e94d370abf484811f14d267dcb1
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2026-01-31 06:46:07 +0800

    refactor(app): remove unused `NavController` parameter

--- app/src/main/kotlin/com/potatosheep/kite/app/ui/KiteAppState.kt
+++ app/src/main/kotlin/com/potatosheep/kite/app/ui/KiteAppState.kt
@@ -2,8 +2,6 @@ package com.potatosheep.kite.app.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.potatosheep.kite.app.navigation.TopLevelDestination
 import com.potatosheep.kite.app.navigation.TopLevelNavKey
 import com.potatosheep.kite.core.navigation.NavigationState
@@ -14,17 +12,13 @@ import com.potatosheep.kite.feature.onboarding.api.navigation.OnboardingNavKey
 @Composable
 fun rememberAppState(
     shouldShowOnboarding: Boolean,
-    navController: NavHostController = rememberNavController(),
 ): KiteAppState {
     val startRoute = if (shouldShowOnboarding) OnboardingNavKey else FeedNavKey
     val navigationState = rememberNavigationState(startRoute, setOf(TopLevelNavKey));
 
-    return remember(
-        navController
-    ) {
+    return remember {
         KiteAppState(
             shouldShowOnboarding = shouldShowOnboarding,
-            navController = navController,
             navigationState = navigationState
         )
     }
@@ -32,7 +26,6 @@ fun rememberAppState(
 
 class KiteAppState(
     val shouldShowOnboarding: Boolean,
-    val navController: NavHostController,
     val navigationState: NavigationState
 ) {
     val topLevelDestinations: List<TopLevelDestination> = TopLevelDestination.entries
