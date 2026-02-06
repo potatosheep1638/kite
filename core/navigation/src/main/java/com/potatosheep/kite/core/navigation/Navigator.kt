5aafeedc97973679d5cdf94c18e76628e2a4f54d
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2026-02-07 05:57:29 +0800

    feat(core): add `navOption` param to `Navigator`

--- core/navigation/src/main/java/com/potatosheep/kite/core/navigation/Navigator.kt
+++ core/navigation/src/main/java/com/potatosheep/kite/core/navigation/Navigator.kt
@@ -6,26 +6,36 @@ import androidx.navigation3.runtime.NavKey
  * Handles navigation events (forward and back) by updating the navigation state.
  */
 class Navigator(val state: NavigationState){
-    fun navigate(route: NavKey, singleTop: Boolean = false){
+    fun navigate(route: NavKey, navOption: NavOption = NavOption.DEFAULT){
         if (route in state.backStacks.keys){
             // This is a top level route, just switch to it.
             state.topLevelRoute = route
         } else {
-            if (singleTop) {
-                val iterator = state.backStacks[state.topLevelRoute]?.listIterator()
+            when (navOption) {
+                NavOption.SINGLE_TOP -> {
+                    val iterator = state.backStacks[state.topLevelRoute]?.listIterator()
 
-                if (iterator != null) {
-                    while (iterator.hasNext()) {
-                        val value = iterator.next()
+                    if (iterator != null) {
+                        while (iterator.hasNext()) {
+                            val value = iterator.next()
 
-                        if (value::class == route::class) {
-                            iterator.remove()
+                            if (value::class == route::class) {
+                                iterator.remove()
+                            }
                         }
                     }
-                }
-            }
 
-            state.backStacks[state.topLevelRoute]?.add(route)
+                    state.backStacks[state.topLevelRoute]?.add(route)
+                }
+
+                NavOption.DESTROY_PREV_NAV -> {
+                    val last = state.backStacks[state.topLevelRoute]?.last()
+                    state.backStacks[state.topLevelRoute]?.add(route)
+                    state.backStacks[state.topLevelRoute]?.remove(last)
+                }
+
+                else -> state.backStacks[state.topLevelRoute]?.add(route)
+            }
         }
     }
 
@@ -41,4 +51,22 @@ class Navigator(val state: NavigationState){
             currentStack.removeLastOrNull()
         }
     }
+}
+
+enum class NavOption {
+    /**
+     * Navigate to this route as normal.
+     */
+    DEFAULT,
+
+    /**
+     * Before navigating to this route, any previous instance of this route in the back stack will
+     * be removed.
+     */
+    SINGLE_TOP,
+
+    /**
+     * After navigating to this route, remove the previous route from the back stack.
+     */
+    DESTROY_PREV_NAV
 }
