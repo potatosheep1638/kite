c92e52e1c810fe6ca4f741a40a49cbb7dfb390de
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2026-02-07 05:25:57 +0800

    fix(feature): fix incorrect `NavKey`

--- feature/search/impl/src/main/kotlin/com/potatosheep/kite/feature/search/impl/navigation/SearchEntryProvider.kt
+++ feature/search/impl/src/main/kotlin/com/potatosheep/kite/feature/search/impl/navigation/SearchEntryProvider.kt
@@ -6,14 +6,14 @@ import androidx.navigation3.runtime.NavKey
 import com.potatosheep.kite.core.designsystem.defaultTransitionSpec
 import com.potatosheep.kite.core.navigation.Navigator
 import com.potatosheep.kite.feature.post.api.navigation.navigateToPost
+import com.potatosheep.kite.feature.search.api.navigation.SearchNavKey
 import com.potatosheep.kite.feature.search.impl.SearchRoute
 import com.potatosheep.kite.feature.search.impl.SearchViewModel
 import com.potatosheep.kite.feature.search.impl.SearchViewModel.Factory
-import com.potatosheep.kite.feature.searchresult.api.navigation.SearchResultNavKey
 import com.potatosheep.kite.feature.searchresult.api.navigation.navigateToSearchResult
 
 fun EntryProviderScope<NavKey>.searchEntry(navigator: Navigator) {
-    entry<SearchResultNavKey>(metadata = defaultTransitionSpec()) { key ->
+    entry<SearchNavKey>(metadata = defaultTransitionSpec()) { key ->
         SearchRoute(
             onBackClick = { navigator.goBack() },
             onPostClick = navigator::navigateToPost,
