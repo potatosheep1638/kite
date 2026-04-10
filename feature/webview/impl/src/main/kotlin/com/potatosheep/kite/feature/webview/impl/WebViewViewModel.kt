a9152efec672c2a301a5408eb97454ead747521f
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2026-04-11 04:18:51 +0800

    fix(feature): add missing annotation for `WebViewViewModel`

--- feature/webview/impl/src/main/kotlin/com/potatosheep/kite/feature/webview/impl/WebViewViewModel.kt
+++ feature/webview/impl/src/main/kotlin/com/potatosheep/kite/feature/webview/impl/WebViewViewModel.kt
@@ -3,12 +3,14 @@ package com.potatosheep.kite.feature.webview.impl
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.potatosheep.kite.core.data.repo.UserConfigRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
+@HiltViewModel
 class WebViewViewModel @Inject constructor(
     private val userConfigRepository: UserConfigRepository,
 ) : ViewModel() {
