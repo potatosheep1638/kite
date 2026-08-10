531179bb87ec491519144b6ec02669918354b2dd
potatosheep1638 <potato_sheep.gonad160@slmails.com>
2026-08-10 23:03:08 +0800

    build(app): bump app version to 0.5.2
    (cherry picked from commit adab240c914e135c9f91b9c5d4a2a42f1ac62622)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 
     defaultConfig {
         applicationId = "com.potatosheep.kite"
-        versionCode = 12
-        versionName = "0.5.1"
+        versionCode = 13
+        versionName = "0.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
